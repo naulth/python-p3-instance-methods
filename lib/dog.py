94db5aab8cf06748c0b8860a87b3e0433d331755
Hayden Nault <naulth@gmail.com>
2023-03-15 10:02:04 -0400

    passing tests

--- lib/dog.py
+++ lib/dog.py
@@ -2,6 +2,8 @@
 
 class Dog:
     # Class body goes here
-
+    def bark(self):
+        print("Woof!")
+    def sit(self):
+        print("The dog is sitting.")
     #Instance method definition
-    pass
